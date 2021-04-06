90328d31ecb8cf977b645bf5740ea99118c5a682
Suhana <52523952+Suhana2268@users.noreply.github.com>
2021-04-06 17:10:51 +0530

    Add files via upload

---
+++ Suffixes.java
@@ -0,0 +1,41 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class Suffixes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter date");
+		int date = sc.nextInt();
+		int endnum = 0;
+		if(date > 31 || date <= 0) {
+			System.out.println("Invalid date");
+		}
+		else {
+			endnum = date % 10;
+			if(date > 3 && date <= 20) {
+				System.out.println(date+"th");
+			}
+			else if(endnum == 1) {
+				System.out.println(date+"st");
+			}
+			else if(endnum == 2) {
+				System.out.println(date+"nd");
+			}
+			else if(endnum == 3) {
+				System.out.println(date+"rd");
+			}
+			else {
+				System.out.println(date+"th");
+			}
+			
+		}
+		
+		
+		
+
+	}
+
+}
